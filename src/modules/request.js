24114648ff4e3291658cf457210afed86cf89c46
NikDm <respectedoff@yandex.ru>
2024-08-19 14:34:12 +0300

    corrections кrequest

--- src/modules/request.js
+++ src/modules/request.js
@@ -1,58 +1,39 @@
 const request = () => {
-    const headerForm = document.getElementById('form1')
-    const headerFormName = document.getElementById('form1-name') // type=text
-    const headerFormEmail = document.getElementById('form1-email') //type=email
-    const headerFormPhone = document.getElementById('form1-phone') //type=tel
+    const headerFormName = document.querySelectorAll('.form-name') // type=text
+    const headerFormEmail = document.querySelectorAll('.form-email') //type=email
+    const headerFormPhone = document.querySelectorAll('.form-phone') //type=tel
 
-    const footerForm = document.getElementById('form2')
-    const footerFormName = document.getElementById('form2-name') // type=text
-    const footerFormEmail = document.getElementById('form2-email') //type=email
-    const footerFormPhone = document.getElementById('form2-phone') //type=tel
+    const footerFormName = document.getElementById('form2-name')
     const footerFormMessage = document.getElementById('form2-message') //placeholder="Ваше сообщение"
 
 
-    headerForm.addEventListener('submit', (e) => {
-        e.preventDefault()
-        let isError = false
-        if (/[^а-яА-Я\s\-]/.test(headerFormName.value)) {
-            isError = true
-        }
-
-        if (/[^a-zA-Z0-9@_.!~*']/g.test(headerFormEmail.value)) {
-            isError = true
-        }
-        if (/[^0-9()\-]/g.test(headerFormPhone.value)) {
-            isError = true
-        }
-
-        if(!isError) {
-            form1.submit();
-        }
+    headerFormName.forEach(elem => {
+        elem.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^а-яА-Я\s\-]/, '')
+        })
     })
 
-    footerForm.addEventListener('submit', (e) => {
-        e.preventDefault()
-        let isError = false
-        if (/[^а-яА-Я\s\-]/.test(footerFormName.value)) {
-            isError = true
-        }
-
-        if (/[^a-zA-Z0-9@_.!~*']/g.test(footerFormEmail.value)) {
-            isError = true
-        }
-        if (/[^0-9()\-]/g.test(footerFormPhone.value)) {
-            isError = true
-        }
-
-        if (/[^а-яА-Я\s-]/.test(footerFormMessage.value)) {
-            isError = true
-        }
-
-        if(!isError) {
-            form2.submit();
-        }
+    headerFormEmail.forEach(elem => {
+        elem.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^a-zA-Z0-9@_.!~*']/g, '')
+        })
     })
+
+    headerFormPhone.forEach(elem => {
+        elem.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^0-9()\-]/g, '')
+        })
+    })
+
+    footerFormName.addEventListener('input', (e) => {
+        e.target.value = e.target.value.replace(/[^а-яА-Я\s\-]/, '')
+    })
+
+    footerFormMessage.addEventListener('input', (e) => {
+        e.target.value = e.target.value.replace(/[^а-яА-Я\s\-]/, '')
+    })
+
+
 }
 
 export default request
-
