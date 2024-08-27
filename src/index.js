7acc6ede0f181758a52df908989b8147a6af7bf8
NikDm <respectedoff@yandex.ru>
2024-08-27 21:19:53 +0300

    mod sendForm

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import tabs from './modules/tabs'
 import slider from './modules/slider'
 import sliderCarousel from './modules/sliderCarousel'
 import calc from './modules/calc'
+import sendForm from "./modules/sendForm";
+
 
 timer('25 august 2024')
 menu()
@@ -17,3 +19,12 @@ tabs()
 slider()
 sliderCarousel()
 calc(100)
+sendForm({
+    formId: 'form1',
+    someElem: [
+        {
+            type: 'block',
+            id: 'total'
+        }
+    ]
+})
